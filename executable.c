a649eb6427cb45a1d13c723cc8c1eb1c6d7429af
Wyatt <cwdh4@umsystem.edu>
2020-10-18 15:11:06 -0500

    well, sigint is working now...trying message queues...

--- executable.c
+++ executable.c
@@ -2,10 +2,55 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/types>
+#include <signal.h>
+
+#define SEC_KEY 0x1234
+#define MSG_KEY 0x2345
+
+typedef struct Clock
+{
+        int sec;
+        int nsec;
+} Clock;
+
+typedef struct msgbuf
+{
+        long mtype;
+        char mtext[10];
+};
+
 
 int main()
 {
-	printf("The child process!\n");
+	int shmid, msgqid;
+	struct Clock *clock, msgbuf msgbuf;
+        msgqid = msgget(MSG_KEY, 0644 | IPC_CREAT);
+        if (msgqid == -1)
+        {
+                perror("shmid get failed");
+                return 1;
+        }
+
+
+	shmid = shmget(SEC_KEY, sizeof(Clock), 0644 | IPC_CREAT);
+        if (shmid == -1)
+        {
+                perror("shmid get failed");
+                return 1;
+        }
+
+        clock = (Clock *) shmat(shmid, NULL, 0);
+        if (clock == (void *) -1)
+        {
+                perror("clock get failed");
+                return 1;
+        }
+	
+	printf("Child clock seconds: %d\n", clock->sec);
 	sleep(2);
 }
 
